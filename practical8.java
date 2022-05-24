d3b3cf351978e17f801c90e96c8ff0694e61472b
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 13:12:44 +0530

    Create practical8.java

---
+++ practical8.java
@@ -0,0 +1,18 @@
+package JAVA;
+import java.util.Scanner;
+class practical8{
+public static void main(String [] args){
+int a=2;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter no: ");
+int n=sc.nextInt();
+while(n>1){
+if(n%a==0){
+System.out.print(a+" ");
+n=n/a;
+}else{
+a++;
+}
+}
+}
+}
