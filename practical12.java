8b0eb90929c490201296ce2c74f2cb1815c521e7
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 19:12:55 +0530

    Update practical12.java

--- practical12.java
+++ practical12.java
@@ -1,7 +1,9 @@
 package JAVA;
 import java.util.Random;
-class practical12 {
-    public static void main(String args[]) {
+class practical12 
+{
+    public static void main(String args[])
+    {
         Random sc = new Random(1000);
         for (int i = 0; i < 100; i++) {
             System.out.printf("%5d", sc.nextInt(49));
