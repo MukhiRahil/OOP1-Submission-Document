55b177b1a68d0e169c124b849f047f0628553847
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 13:24:23 +0530

    Update and rename practical5.java to practical05.java

--- practical05.java
+++ practical05.java
@@ -1,6 +1,6 @@
 package JAVA;
 import java.util.Scanner;
-class practical5{
+class practical05{
 public static void main(String args []){
 Scanner sc=new Scanner(System.in);
 System.out.print("Enter three digits: ");
