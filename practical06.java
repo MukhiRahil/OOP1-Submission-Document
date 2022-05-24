da6db5f52d3d13a42ec469c3e2b840d1109029ef
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 18:40:31 +0530

    Update practical06.java

--- practical06.java
+++ practical06.java
@@ -2,13 +2,15 @@ package JAVA;
 import java.util.Scanner;
 class practical06
 {
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Character : ");
         char ch = sc.next().charAt(0);
         if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             System.out.println("Given Character is Vowel ");
-        } else {
+        } 
+        else {
             System.out.println("Given Character is Consonant ");
         }
     }
