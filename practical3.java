fc85d3c0dff4769875c47460098d54a61d0fdec4
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 12:29:49 +0530

    Create practical3.java

---
+++ practical3.java
@@ -0,0 +1,13 @@
+package JAVA;
+import java.util.Scanner;
+public class practical3
+{
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+double meter;
+System.out.println("Enter the Any Value in Meter:- ");
+meter = sc.nextDouble();
+double Feet = (meter * 3.28084) ;
+System.out.println("Meter = " + meter + " Feet = " +Feet);
+}
+}
