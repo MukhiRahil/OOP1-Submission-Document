fbd28aca2b4a70b81fb0c8165692c77559d7b4f1
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 12:30:28 +0530

    Create practical4.java

---
+++ practical4.java
@@ -0,0 +1,12 @@
+package JAVA;
+import java.util.Scanner;
+class practical4{
+public static void main(String args []){
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter weight (in pounds) :");
+double w=sc.nextDouble();
+System.out.print("Enter Height (in inches) :");
+double h=sc.nextDouble();
+System.out.println("BMI : "+ (w*0.4539237)/(h*0.254*h*0.254));
+}
+}
