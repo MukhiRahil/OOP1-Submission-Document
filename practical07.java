fc3d0b2f1356af50e41c3cb2c5b8c06914aa5ed7
Mukhi Rahil <103422017+MukhiRahil@users.noreply.github.com>
2022-05-24 18:41:49 +0530

    Update practical07.java

--- practical07.java
+++ practical07.java
@@ -1,7 +1,8 @@
 package JAVA;
 class practical07
 {
-    public static void main(String args[]) {
+    public static void main(String args[]) 
+    {
         System.out.print("plate No: ");
         System.out.print((char) ((int) (Math.random() * (90 - 65) + 65)));
         System.out.print((char) ((int) (Math.random() * (90 - 65) + 65)));
